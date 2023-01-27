175f5f1d8dbceebd4fb5b2c1b29096e2c9a4528e
KimGwanWoo <dndnwnan@gmail.com>
2023-01-28 08:47:32 +0900

    first commit

---
+++ POST_VASP_PROJECT/BAND_STRUCTURE/Band+DOS_Fin.c
@@ -0,0 +1,112 @@
+dDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\profapi.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheScheduleImports: Import todo: #0 'msvcrt.dll'.
+3b70.4608: supR3HardenedWinVerifyCacheScheduleImports: Import todo: #8 'crypt32.dll'.
+3b70.4608: supHardenedWinVerifyImageByHandle: -> 0 (\Device\HarddiskVolume3\Windows\System32\cryptnet.dll)
+3b70.4608: supR3HardenedWinVerifyCacheInsert: \Device\HarddiskVolume3\Windows\System32\cryptnet.dll
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'crypt32.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'crypt32.dll' -> '\Device\HarddiskVolume3\Windows\System32\crypt32.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'msvcrt.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'msvcrt.dll' -> '\Device\HarddiskVolume3\Windows\System32\msvcrt.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\msvcrt.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'rpcrt4.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'rpcrt4.dll' -> '\Device\HarddiskVolume3\Windows\System32\rpcrt4.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rpcrt4.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'msvcrt.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'msvcrt.dll' -> '\Device\HarddiskVolume3\Windows\System32\msvcrt.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\msvcrt.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'rpcrt4.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'rpcrt4.dll' -> '\Device\HarddiskVolume3\Windows\System32\rpcrt4.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rpcrt4.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (Input=cryptnet.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedScreenImage/NtCreateSection: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedDllNotificationCallback: load   00007ffde48b0000 LB 0x00031000 C:\Windows\System32\cryptnet.dll [fFlags=0x0]
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\cryptnet.dll (rcNtResolve=0xc0150008) *pfFlags=0x2 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\cryptnet.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde48b0000 'C:\Windows\System32\cryptnet.dll'
+3b70.4608: supR3HardenedDllNotificationCallback: load   00007ffdec2f0000 LB 0x000ac000 C:\Windows\System32\advapi32.dll [fFlags=0x0]
+3b70.4608: supR3HardenedWinVerifyCacheScheduleImports: Import todo: #0 'msvcrt.dll'.
+3b70.4608: supR3HardenedWinVerifyCacheScheduleImports: Import todo: #6 'sechost.dll'.
+3b70.4608: supR3HardenedWinVerifyCacheScheduleImports: Import todo: #27 'rpcrt4.dll'.
+3b70.4608: supHardenedWinVerifyImageByHandle: -> 0 (\Device\HarddiskVolume3\Windows\System32\advapi32.dll)
+3b70.4608: supR3HardenedWinVerifyCacheInsert: \Device\HarddiskVolume3\Windows\System32\advapi32.dll
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rsaenh.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'rpcrt4.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'rpcrt4.dll' -> '\Device\HarddiskVolume3\Windows\System32\rpcrt4.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rpcrt4.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'sechost.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'sechost.dll' -> '\Device\HarddiskVolume3\Windows\System32\sechost.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\sechost.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: Processing 'msvcrt.dll'...
+3b70.4608: supR3HardenedWinVerifyCacheProcessImportTodos: 'msvcrt.dll' -> '\Device\HarddiskVolume3\Windows\System32\msvcrt.dll' [rcNtRedir=0xc0150008]
+3b70.4608: supR3HardenedScreenImage/Imports: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\msvcrt.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\system32\rsaenh.dll (rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde8dc0000 'C:\Windows\system32\rsaenh.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\crypt32.dll (Input=crypt32.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffdea470000 'C:\Windows\System32\crypt32.dll'
+3b70.4608: supR3HardNtViCallWinVerifyTrustCatFile: hFile=0000000000000000 pwszName=\SystemRoot\System32\ntdll.dll
+3b70.4608: supR3HardNtViCallWinVerifyTrustCatFile: New context 00000000011e1a10
+3b70.4608: supR3HardNtViCallWinVerifyTrustCatFile: hCatAdmin=00000000011e1a10
+3b70.4608: supR3HardNtViCallWinVerifyTrustCatFile: cbHash=20 wszDigest=CF5EACF36F78DD76A9C15BF564DC1094C86C4B18
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rpcrt4.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\rpcrt4.dll (Input=rpcrt4.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffdea6a0000 'C:\Windows\System32\rpcrt4.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rsaenh.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\system32\rsaenh.dll (rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde8dc0000 'C:\Windows\system32\rsaenh.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\crypt32.dll (Input=crypt32.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffdea470000 'C:\Windows\System32\crypt32.dll'
+3b70.4608: supR3HardNtViCallWinVerifyTrustCatFile: WinVerifyTrust => 0x0; cat='C:\Windows\system32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\Microsoft-Windows-Client-Desktop-Required-Package0516~31bf3856ad364e35~amd64~~10.0.19041.1348.cat'; file='\SystemRoot\System32\ntdll.dll'
+3b70.4608: g_pfnWinVerifyTrust=00007ffde9e71da0
+3b70.4608: supR3HardenedScreenImage/preload: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll [redoing WinVerifyTrust]
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rsaenh.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\system32\rsaenh.dll (rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde8dc0000 'C:\Windows\system32\rsaenh.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\crypt32.dll (Input=crypt32.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffdea470000 'C:\Windows\System32\crypt32.dll'
+3b70.4608: supR3HardenedScreenImage/preload: 0 (was 0) fWinVerifyTrust=1 for '\Device\HarddiskVolume3\Windows\System32\crypt32.dll'
+3b70.4608: supR3HardenedScreenImage/preload: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\wintrust.dll [redoing WinVerifyTrust]
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rsaenh.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\system32\rsaenh.dll (rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde8dc0000 'C:\Windows\system32\rsaenh.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\System32\crypt32.dll (Input=crypt32.dll, rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffdea470000 'C:\Windows\System32\crypt32.dll'
+3b70.4608: supR3HardenedScreenImage/preload: 0 (was 0) fWinVerifyTrust=1 for '\Device\HarddiskVolume3\Windows\System32\wintrust.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\rsaenh.dll [lacks WinVerifyTrust]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Windows\system32\rsaenh.dll (rcNtResolve=0xc0150008) *pfFlags=0x0 pwszSearchPath=0000000000000001:<flags> [calling]
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: returns rcNt=0x0 hMod=00007ffde8dc0000 'C:\Windows\system32\rsaenh.dll'
+3b70.4608: supR3HardenedScreenImage/LdrLoadDll: cache hit (VINF_SUCCESS) on \Device\HarddiskVolume3\Windows\System32\crypt32.dll
+3b70.4608: supR3HardenedMonitor_LdrLoadDll: pName=C:\Window
